3fa3577b74c9680ace02633954b9b11cd68f376a
Nick McMahon <npm.mcmahon@gmail.com>
2018-03-06 23:49:07 -0500

    Improves code, adds travis

--- index.js
+++ index.js
@@ -10,20 +10,20 @@ module.exports = function (homebridge) {
 
 function garageDoor(log, config) {
   this.log = log;
-  var urlString = config['piUrl'];
-  var port = config['listen_port'];
+  var urlString = config.piUrl;
+  var port = config.listen_port;
   if (!port) {
-    port = 8080
+    port = 8080;
   }
   if (!urlString.endsWith("/")){
-    urlString += "/"
+    urlString += "/";
   }
   this.piUrlString = urlString;
   this.port = port;
   this.values = {
     "Current Position": 0,
     "Target Position": 0
-  }
+  };
 }
 
 garageDoor.prototype = {
@@ -56,7 +56,7 @@ garageDoor.prototype = {
     this.sendRequest(next, "status?field=currentState", "currentState");
   },
   setCurrentDoorState: function(state, next) {
-    this.sendRequest(next, "control?currentState=" + state)
+    this.sendRequest(next, "control?currentState=" + state);
   },
   getObstructions: function(next) {
     next(null, false);
@@ -65,14 +65,14 @@ garageDoor.prototype = {
     next(null);
   },
   getTargetDoorState: function(next) {
-    this.sendRequest(next, "status?field=targetState", "targetState")
+    this.sendRequest(next, "status?field=targetState", "targetState");
   },
   setTargetDoorState: function (targetPos, next) {
-    this.sendRequest(next, "control?targetState=" + targetPos)
+    this.sendRequest(next, "control?targetState=" + targetPos);
   },
   sendRequest: function(callback, path, field) {
     var urlString = this.piUrlString + path;
-    var requestUrl = url.parse(urlString)
+    var requestUrl = url.parse(urlString);
 
     if (!field){
       this.log("Sending command: " + urlString);
@@ -89,7 +89,7 @@ garageDoor.prototype = {
       }
       if (body && body.length > 0){
         var newValue = JSON.parse(body)[field];
-        this.log("Fetched Garage Door Status [" + field + "]: " + newValue)
+        this.log("Fetched Garage Door Status [" + field + "]: " + newValue);
         return callback(null, newValue);
       } else {
         return callback(null);
@@ -97,7 +97,7 @@ garageDoor.prototype = {
     }.bind(this));
   },
   startServer: function() {
-    this.log("Listening for changes on port " + this.port)
+    this.log("Listening for changes on port " + this.port);
     var server = http.createServer(function (request, response) {
       var body = "";
 
@@ -108,9 +108,9 @@ garageDoor.prototype = {
      
       request.on('end', function () {
         var queryData = JSON.parse(body);
-        this.tryUpdateValue(Characteristic.TargetDoorState, queryData.targetState)
-        this.tryUpdateValue(Characteristic.CurrentDoorState, queryData.currentState)
-        this.tryUpdateValue(Characteristic.ObstructionDetected, queryData.obstructionDetected)
+        this.tryUpdateValue(Characteristic.TargetDoorState, queryData.targetState);
+        this.tryUpdateValue(Characteristic.CurrentDoorState, queryData.currentState);
+        this.tryUpdateValue(Characteristic.ObstructionDetected, queryData.obstructionDetected);
         response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.end('ok');
       }.bind(this));
@@ -119,7 +119,7 @@ garageDoor.prototype = {
   tryUpdateValue: function(field, value) {
     if (value != null && value != undefined) {
       var characteristic = this.doorService.getCharacteristic(field);
-      this.log("Received \"" + characteristic.displayName + "\" with value \"" + value + "\"")
+      this.log("Received \"" + characteristic.displayName + "\" with value \"" + value + "\"");
       this.values[characteristic.displayName] = value;
       characteristic.updateValue(value);
     }
